4b124d235264325d3f37f4acc0099b2f117342aa
elyasafko <elyasafko@gmail.com>
2026-01-09 12:12:14 +0200

    Add health and version endpoints, update app version to 1.2, and enhance deployment script

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
-from flask import Flask
+from flask import Flask, jsonify
+import socket
+from datetime import datetime, timezone
 
 app = Flask(__name__)
 
+APP_VERSION = "1.2"
+
 @app.route('/')
 def hello():
-    return "Hello World! Version 1.1"
+    return "Hello World!"
+
+@app.route('/health')
+def health():
+    return "OK", 200
+
+@app.route('/version')
+def version():
+    return APP_VERSION , 200
+
+@app.route('/info')
+def info():
+    data = {
+        "hostname": socket.gethostname(),
+        "server_time_utc": datetime.now(timezone.utc).isoformat(),
+        "server_time_local": datetime.now().astimezone().isoformat(),
+        "app": "sample-flask-app"
+    }
+    return jsonify(data), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, ssl_context='adhoc')
+    #app.run(host='0.0.0.0', port=5000, ssl_context='adhoc') # for HTTPS
+    app.run(host='0.0.0.0', port=5000)
---
+++ tests/test_app.py
@@ -0,0 +1,13 @@
+from app import app
+
+def test_root():
+    client = app.test_client()
+    res = client.get("/")
+    assert res.status_code == 200
+    assert b"Hello World!" in res.data
+
+def test_health():
+    client = app.test_client()
+    res = client.get("/health")
+    assert res.status_code == 200
+    assert b"OK" in res.data
