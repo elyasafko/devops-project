302645601e8a27998d14dcd9eb883ec5598a4e1d
elyasafko <elyasafko@gmail.com>
2026-01-12 23:21:19 +0200

    Refactor app.py for consistent string formatting and improve code readability

--- app.py
+++ app.py
@@ -7,28 +7,33 @@ app = Flask(__name__)
 
 APP_VERSION = "1.2"
 
-@app.route('/')
+
+@app.route("/")
 def hello():
     return "Hello World! you can also check /health, /version, /info endpoints.", 200
 
-@app.route('/health')
+
+@app.route("/health")
 def health():
     return "OK", 200
 
-@app.route('/version')
-def version():
-    return APP_VERSION , 200
 
-@app.route('/info')
+@app.route("/version")
+def version():
+    return APP_VERSION, 200
+
+
+@app.route("/info")
 def info():
     data = {
         "hostname": socket.gethostname(),
         "server_time_utc": datetime.now(timezone.utc).isoformat(),
         "server_time_local": datetime.now().astimezone().isoformat(),
-        "app": "sample-flask-app"
+        "app": "sample-flask-app",
     }
     return jsonify(data), 200
 
-if __name__ == '__main__':
-    #app.run(host='0.0.0.0', port=5000, ssl_context='adhoc') # for HTTPS
-    app.run(host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    # app.run(host='0.0.0.0', port=5000, ssl_context='adhoc') # for HTTPS
+    app.run(host="0.0.0.0", port=5000)
